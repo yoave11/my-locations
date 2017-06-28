dc48220a2b9c2185281e44a2390648e75ea5a729
yoave11 <yoave11@yahoo.com>
2017-06-29 00:02:40 +0300

    make it better

--- src/app/location.ts
+++ src/app/location.ts
@@ -1,5 +1,6 @@
 import {Category} from "./category";
 import {Coordinates} from "./coordinates";
 export class Location {
+
   constructor(public name:string,public address:string,public coordinates:Coordinates,public category:Category){}
 }
